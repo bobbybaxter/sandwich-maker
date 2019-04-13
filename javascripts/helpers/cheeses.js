02373a1f0c16e92b060a52a3fd38dfd484a7ad9b
Bob <bob.e.baxter@gmal.com>
2019-04-13 10:48:15 -0500

    added cheese module

---
+++ javascripts/helpers/cheeses.js
@@ -0,0 +1,13 @@
+const allCheeses = {
+  cheddar: .25,
+  provalone: .25,
+  swiss: .25,
+  pepperJack: .25,
+  montereyJack: .25
+};
+
+const addCheese = () => {
+  return allCheeses;
+};
+
+export default { addCheese };
