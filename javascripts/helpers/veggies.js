eaa4d6e9ed5756511463266a1b09fc8888c87df2
Bob <bob.e.baxter@gmal.com>
2019-04-13 10:52:37 -0500

    added veggies module

---
+++ javascripts/helpers/veggies.js
@@ -0,0 +1,13 @@
+const allVeggies = {
+  tomatoes: .10,
+  lettuce: .10,
+  redOnions: .10,
+  jalapenos: .10,
+  greenPeppers: .10
+};
+
+const addVeggie = () => {
+  return allVeggies;
+};
+
+export default { addVeggie };
