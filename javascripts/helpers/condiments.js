811c8f2e119915f0ebfc881ee3123dc59f6d65ef
Bob <bob.e.baxter@gmal.com>
2019-04-13 10:58:18 -0500

    added condiments, updated meat prices

---
+++ javascripts/helpers/condiments.js
@@ -0,0 +1,13 @@
+const allCondiments = {
+  mayonnaise: .10,
+  mustard: .10,
+  parmeseanCheese: .10,
+  oilAndVinegar: .10,
+  tzatzikiCucumber: .10
+};
+
+const addCondiment = () => {
+  return allCondiments;
+};
+
+export default { addCondiment };
