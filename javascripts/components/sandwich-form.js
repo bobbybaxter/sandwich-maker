c46f902437548e4e5d663bbb1c116730df1546eb
Bob <bob.e.baxter@gmal.com>
2019-04-14 17:25:44 -0500

    sandwich form is ugly, but works as needed per ticket

--- javascripts/components/sandwich-form.js
+++ javascripts/components/sandwich-form.js
@@ -16,28 +16,33 @@ const pendingSandwich = {
   items: [],
   price: 0
 };
+let idCounter = 0;
 
-// test shopping cart, will be moved to cart.js when built
-const addToCart = (array) => {
+// test function, used only to show that items are building behind the scenes
+const buildSandwich = (array) => {
   let domString = '';
   array.forEach(currentItem => {
-    domString += `<h3>${array}</h3>`    
+    domString += `<h3>${currentItem}</h3>`    
   });
-  util.printToDom('shopping-cart', domString);
-}
+  util.printToDom('test-items', domString);
+};
 
 const itemToggle = (e) => {
   const id = e.target;
   const itemName = e.target.nextElementSibling.innerText;
   const itemPrice = e.target.nextElementSibling.nextElementSibling.innerText;
   if (id.checked) {
-    pendingSandwich.items.push(e.target.nextElementSibling.innerText);
-    buildCart(pendingSandwich.items);
+    pendingSandwich.items.push(itemName);
+    pendingSandwich.price += Number(itemPrice);
+    console.log(pendingSandwich);
+    buildSandwich(pendingSandwich.items);
   } else {
     pendingSandwich.items.forEach((ingredient, index) => {
       if (ingredient === itemName) {
         pendingSandwich.items.splice(index, 1);
-        addToCart(pendingSandwich.items);
+        pendingSandwich.price -= Number(itemPrice);
+        console.log(pendingSandwich);
+        buildSandwich(pendingSandwich.items);
       };
     });
   };
@@ -47,18 +52,23 @@ const ingredientBuilder = (printLoc, object) => {
   const objects = Object.entries(object);
   let domString = '';
   for (const [key, property] of objects) {
-    // console.log(key, property[0], property[1]);
     domString += `<div class="ingredient-box">`;
     domString += `  <input type="checkbox" aria-checked="false" class="ingredient-selector" id="${key}"></input>`;
     domString += `  <p>${property[0]}</p>`;
-    domString += `  <p>${property[1]}</p>`;
+    domString += `  <p>${property[1].toFixed(2)}</p>`;
     domString += `</div>`;
     document.getElementById('divProductOptions').addEventListener('change', itemToggle);
   };
   util.printToDom(printLoc, domString);
-  // for (const key of objects) {
-  //   console.log('key', key);
-  // };
+};
+
+// test function, possibly be moved to cart.js when built
+const addToCart = (e) => {
+  util.printToDomAdd('shopping-cart', pendingSandwich.items);
+  util.printToDomAdd('shopping-cart', pendingSandwich.price);
+  let allCheckboxes = document.querySelectorAll("input.ingredient-selector");
+  allCheckboxes.checked = false;
+  makeSandwichForm();
 };
 
 const makeSandwichForm = () => {
@@ -69,9 +79,8 @@ const makeSandwichForm = () => {
   ingredientBuilder('condimentDiv', condiments);
 };
 
-// const eventListeners = () => {
-//   // document.getElementsByClassName('ingredient-selector').addEventListener('change', itemToggle);
-// }
+const eventListeners = () => {
+  document.getElementById('add-to-cart-button').addEventListener('click', addToCart);
+};
 
-
-export default { makeSandwichForm };
+export default { makeSandwichForm, eventListeners };
--- javascripts/helpers/util.js
+++ javascripts/helpers/util.js
@@ -3,6 +3,11 @@ const printToDom = (divId, textToPrint) => {
   selectedDiv.innerHTML = textToPrint;
 };
 
+const printToDomAdd = (divId, textToPrint) => {
+  let selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML += textToPrint;
+};
+
 const varToString = varObj => Object.keys(varObj)[0];
 
-export default { printToDom, varToString };
+export default { printToDom, printToDomAdd, varToString };
